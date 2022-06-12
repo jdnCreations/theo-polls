456decf6fcf77e035d6869170a9a7ee1044df9de
Jordan Needham <jdncreations@hotmail.com>
2022-06-12 19:32:18 +0800

    tRPC working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { withTRPC } from '@trpc/next';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { AppRouter } from './api/trpc/[trpc]';
 
-export default MyApp
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  // try false if issues
+  ssr: true,
+})(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import { prisma } from '../db/client';
+import { trpc } from '../utils/trpc';
 
 export default function Home(props: any) {
-  return (
-    <div>
-      <Head>
-        <title>Home</title>
-        <meta name='description' content='This is the home page' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main>
-        <h1 className='text-2xl font-light'>WELCOME ABOARD.</h1>
-        <code>{props.questions}</code>
-      </main>
-    </div>
-  );
+  const { data, isLoading } = trpc.useQuery(['hi']);
+
+  if (isLoading || !data) {
+    return <div>Loading...</div>;
+  }
+  return <div>{data?.greeting}</div>;
 }
 
 export const getServerSideProps = async () => {
