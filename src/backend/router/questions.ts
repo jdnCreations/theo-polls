7efb9883493d4e4fbb16bc310ddf43a8e86cdd10
Jordan Needham <jdncreations@hotmail.com>
2022-06-14 12:15:11 +0800

    Fix votes

--- src/backend/router/questions.ts
+++ src/backend/router/questions.ts
@@ -40,6 +40,9 @@ export const questionRouter = createRouter()
 
       if (rest.vote || rest.isOwner) {
         const votes = await prisma?.vote.groupBy({
+          where: {
+            questionId: input.id,
+          },
           by: ['choice'],
           _count: true,
         });
