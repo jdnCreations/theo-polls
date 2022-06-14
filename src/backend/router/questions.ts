f9c66317e7e5b2cda3158973601dae25788dc665
Jordan Needham <jdncreations@hotmail.com>
2022-06-14 12:12:35 +0800

    Working voting

--- src/backend/router/questions.ts
+++ src/backend/router/questions.ts
@@ -32,11 +32,25 @@ export const questionRouter = createRouter()
         },
       });
 
-      return {
+      const rest = {
         question,
         vote: myVote,
         isOwner: question?.ownerToken === ctx.token,
       };
+
+      if (rest.vote || rest.isOwner) {
+        const votes = await prisma?.vote.groupBy({
+          by: ['choice'],
+          _count: true,
+        });
+
+        return {
+          ...rest,
+          votes,
+        };
+      }
+
+      return { ...rest, votes: undefined };
     },
   })
   .mutation('vote-on-question', {
